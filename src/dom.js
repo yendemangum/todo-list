00990559ae91ddeb13533fc856d095c04cc051a4
Yende Mangum <yendemangum@gmail.com>
2024-10-17 14:29:11 -0400

    Update todo page to include delete, no storage

--- src/dom.js
+++ src/dom.js
@@ -2,9 +2,12 @@
 todo as form 
 form contents to divs 
 append to container div
+
+create project with new todo
+list contents from project.array
 */
 
-import { createTodo, createProject } from "./todo.js"
+import { createTodo, createProject, Project } from "./todo.js"
 
 const body = document.querySelector("body")
 const form = document.querySelector(".form")
@@ -17,36 +20,52 @@ newProjectButton.textContent = "New Project"
 
 container.prepend(newProjectButton)
 
-const defaultProject = document.createElement("div")
-defaultProject.setAttribute("class", "project")
+const defaultProject = newProj()
 
 
 let currentProject = defaultProject
+
 function runDisplay() {
     content.textContent = ""
-    if (currentProject.firstChild) {
-    for (let i = 0; i < currentProject.childNodes.length; i++) {
-        if (currentProject.childNodes[i].textContent !== "") {
-        const newField = document.createElement("div")
-        newField.setAttribute("class", "field")
-        newField.textContent = currentProject.childNodes[i].textContent
-        content.append(newField)}
+    if (Object.keys(currentProject).length !== 0) {
+        for (let i = 0; i < currentProject.array.length; i++) {
+            const fieldBox = document.createElement("div")
+            fieldBox.setAttribute("class", "field-box")
+            if (currentProject.array[i] != "") {
+                for (let key in currentProject.array[i]) {
+                    const newField = document.createElement("div")
+                    newField.setAttribute("class", "field")
+                    newField.textContent = currentProject.array[i][key]
+                    fieldBox.append(newField)
+                }
+            }
+            const deleteTodoButton = document.createElement("button")
+            deleteTodoButton.textContent = "Delete Todo"
+            deleteTodoButton.setAttribute('class', "dtb")
+            deleteTodoButton.addEventListener("click", () => {
+                currentProject.array.splice(i, 1)
+                runDisplay()
+            })
+            fieldBox.append(deleteTodoButton)
+            content.append(fieldBox)
+        }
     }
-    }
+    console.log(currentProject.array)
 }
 
 runDisplay()
 newProjectButton.addEventListener("click", addProject)
 
 function addProject() {
-    content.append(currentProject)
+
     const projectName = prompt("Enter a name for your project", "Project Name")
-    const newProject = document.createElement("div")
-    newProject.setAttribute("class", "project")
+    const newProject = newProj()
+
     const projectButton = document.createElement("button")
     projectButton.setAttribute("class", "project-button")
     projectButton.textContent = projectName
     container.prepend(projectButton)
+
     projectButton.addEventListener("click", () => {
         currentProject = newProject
         runDisplay()
@@ -55,30 +74,34 @@ function addProject() {
 }
 
 
+function newProj() {
+
+    const project = createProject()
+    return project
+
+}
+
+function newTodo() {
+    const formData = new FormData(form);
+    const todo = createTodo(formData.get("title"), formData.get("description"), formData.get("date"), formData.get("priority"));
+    return todo
+
+}
+
+
+
+
+
 form.addEventListener("submit", (event) => {
     event.preventDefault()
 
-    const formData = new FormData(form);
-    const todo = createTodo(formData.get("title"), formData.get("description"), formData.get("date"), formData.get("priority"));
-
-    const field1 = document.createElement("div")
-    field1.setAttribute("class", "field")
-    field1.textContent = todo.title
-    const field2 = document.createElement("div")
-    field2.setAttribute("class", "field")
-    field2.textContent = todo.description
-    const field3 = document.createElement("div")
-    field3.setAttribute("class", "field")
-    field3.textContent = todo.dueDate
-    const field4 = document.createElement("div")
-    field4.setAttribute("class", "field")
-    field4.textContent = todo.priority + " priority"
+    const todo = newTodo()
 
     form.style.display = "none";
     body.style.backgroundColor = "antiquewhite"
 
-    currentProject.append(field1, field2, field3, field4)
-    console.log(currentProject)
+    currentProject.addToProject(todo)
+
     runDisplay()
 })
 
@@ -102,11 +125,6 @@ newTodoButton.addEventListener("click", () => {
     body.style.backgroundColor = "lightgray"
     runDisplay()
 })
-
-
-
-
-
 
 
 
